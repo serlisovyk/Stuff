6ca0c8faeecfdfb8df44700e91c8985472e92706
Serhii Lisovik <serlesovik@gmail.com>
2025-01-12 18:50:14 +0200

    Updated zustand cart store

--- client/src/state/zustand/useCartStore.js
+++ client/src/state/zustand/useCartStore.js
@@ -1,27 +1,48 @@
 import { create } from 'zustand'
 import { immer } from 'zustand/middleware/immer'
 
+// Utility for updating the cart
+const updateCart = (cart, item) => {
+  const found = cart.find(cartItem => cartItem._id === item._id)
+
+  if (found) {
+    if (item.quantity <= 0) {
+      // If the quantity is less than or equal to 0, remove the item
+      return cart.filter(cartItem => cartItem._id !== item._id)
+    }
+
+    // Update the quantity of the existing item
+    return cart.map(cartItem =>
+      cartItem._id === item._id ? { ...cartItem, quantity: item.quantity } : cartItem
+    )
+  }
+
+  // If the item is not in the cart, add it with a quantity of 1
+  return [...cart, { ...item, quantity: 1 }]
+}
+
+// Utility for removing an item from the cart
+const removeFromCart = (cart, _id) => cart.filter(item => item._id !== _id)
+
 export const useCartStore = create(
   immer(set => ({
     cart: [],
 
+    // Method for adding an item to the cart
     addItemToCart: item =>
-      set(state => {
-        let newCart = [...state.cart]
-        const found = state.cart.find(cartItem => cartItem._id === item._id)
+      set(state => ({
+        cart: updateCart(state.cart, item),
+      })),
 
-        found
-          ? (newCart = newCart.map(cartItem =>
-              cartItem._id === item._id
-                ? { ...cartItem, quantity: item.quantity }
-                : cartItem
-            ))
-          : newCart.push({ ...item, quantity: 1 })
-
-        state.cart = newCart
-      }),
-
+    // Method for removing an item from the cart
     removeItemFromCart: _id =>
-      set(state => ({ cart: state.cart.filter(item => item._id !== _id) })),
+      set(state => ({
+        cart: removeFromCart(state.cart, _id),
+      })),
   }))
 )
+
+// Selectors
+export const selectCart = state => state.cart
+export const selectAddItemToCart = state => state.addItemToCart
+export const selectRemoveItemFromCart = state => state.removeItemFromCart
